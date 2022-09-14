6722cc84b033018a5ef722a8bad26be500cf0ced
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-09-15 00:30:49 +0300

    Add project files.

---
+++ DataAccessLayer/EntityFramework/EfProductDAL.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfProductDAL : GenericRepository<Product>, IProductDAL
+    {
+        //güncelleme için gereken işlemler
+        public void ChangeStatusToFalse(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+        public void Delete(T item)
+        {
+            _object.Remove(item);
+            c.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(T item)
+        {
+            _object.Add(item);
+            c.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            c.SaveChanges();
+        }
+    }
+}
